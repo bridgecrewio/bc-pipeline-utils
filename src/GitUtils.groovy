95ee28f4cac10dc3e83b6b5f0b5faa6159c61294
Or Evron <orevron@gmail.com>
2019-10-27 15:55:15 +0200

    moved file

--- src/GitUtils.groovy
+++ src/GitUtils.groovy
@@ -12,13 +12,13 @@ class GitUtils {
         sh("ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts")
     }
 
-    def gitCommandWithCredentials(command, credentials) {
-        this.script.withCredentials([this.script.sshUserPrivateKey(credentialsId: credentials, keyFileVariable: 'ssh_key', passphraseVariable: '', usernameVariable: 'bridgecrew')]) {
-            this.script.sh("cp ${this.script.ssh_key} ~/.ssh/id_rsa_write")
-            this.script.sh("chmod 400 ~/.ssh/id_rsa_write")
-            this.script.sh("GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa_write' git ${command}")
-        }
-    }
+//    def gitCommandWithCredentials(command, credentials) {
+//        this.script.withCredentials([this.script.sshUserPrivateKey(credentialsId: credentials, keyFileVariable: 'ssh_key', passphraseVariable: '', usernameVariable: 'bridgecrew')]) {
+//            this.script.sh("cp ${this.script.ssh_key} ~/.ssh/id_rsa_write")
+//            this.script.sh("chmod 400 ~/.ssh/id_rsa_write")
+//            this.script.sh("GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa_write' git ${command}")
+//        }
+//    }
 
     def mycheckout(String url, String branch) {
         checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: "${url}"]]])
