3c6441c1dcb64be84cd6aa266b69cf21a7470b66
Or Evron <orevron@gmail.com>
2019-10-28 17:24:02 +0200

    added aws util

--- src/AWS.groovy
+++ src/AWS.groovy
@@ -1,14 +1,35 @@
-def call(awsAccount) {
-    println "Running aws-nuke for profile: ${awsAccount}"
-    def accountId = accountFromProfile(awsAccount)
-    sh """
+class AWS {
+    def script
+    String credentials
+
+    AWS(def script, String credentials) {
+        this.script = script
+        this.credentials = credentials
+    }
+
+    def configure(){
+        this.script.withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: this.credentials, accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+            script {
+                this.script.sh "aws configure --profile ${this.credentials} set aws_access_key_id ${this.script.AWS_ACCESS_KEY_ID}"
+                this.script.sh "aws configure --profile ${this.credentials} set aws_secret_access_key ${this.script.AWS_SECRET_ACCESS_KEY}"
+                this.script.sh "aws configure --profile ${this.credentials} set region us-west-2"
+            }
+        }
+    }
+
+
+
+    def nukeAccount(awsAccount) {
+        println "Running aws-nuke for profile: ${awsAccount}"
+        def accountId = this.script.accountFromProfile(awsAccount)
+        this.script.sh """
             wget https://github.com/rebuy-de/aws-nuke/releases/download/v2.10.0/aws-nuke-v2.10.0-linux-amd64.tar.gz -O ./aws-nuke.tar.gz
             tar -xzf aws-nuke.tar.gz
             mv aws-nuke-v2.10.0-linux-amd64 aws-nuke
             ls -la
             sudo chmod +x ./aws-nuke
         """
-    sh """                        
+        this.script.sh """                        
             cat << EOF > nuke-config.yaml
                 regions:
                 - "global"
@@ -76,14 +97,15 @@ def call(awsAccount) {
                        SSMParameter:
                        - "/base_stack/ses_support_arn"
           """
-    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: env.AWS_BASE_PROFILE, accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-        script {
-            sh "aws configure --profile ${awsAccount} set aws_access_key_id ${AWS_ACCESS_KEY_ID}"
-            sh "aws configure --profile ${awsAccount} set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}"
-            sh "aws configure --profile ${awsAccount} set region us-west-2"
-            sh "ls -la"
-            sh "cat nuke-config.yaml"
-            sh "./aws-nuke -c nuke-config.yaml --profile ${awsAccount} --no-dry-run --force"
+        this.script.withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: this.credentials, accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+            script {
+                this.script.sh "aws configure --profile ${awsAccount} set aws_access_key_id ${this.script.AWS_ACCESS_KEY_ID}"
+                this.script.sh "aws configure --profile ${awsAccount} set aws_secret_access_key ${this.script.AWS_SECRET_ACCESS_KEY}"
+                this.script.sh "aws configure --profile ${awsAccount} set region us-west-2"
+                this.script.sh "ls -la"
+                this.script.sh "cat nuke-config.yaml"
+                this.script.sh "./aws-nuke -c nuke-config.yaml --profile ${awsAccount} --no-dry-run --force"
+            }
         }
     }
 }
