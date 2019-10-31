499bee9025c01583d884ebb89bdf5ca8ebd36bad
Or Evron <orevron@gmail.com>
2019-10-31 15:23:20 +0200

    logs

--- vars/bumpVersion.groovy
+++ vars/bumpVersion.groovy
@@ -1,7 +1,10 @@
 def call(){
+    println "bumping version"
     def packageJson = readJSON file: "package.json"
+    println "current version ${packageJson['version']}"
     def version = packageJson['version'].split('\\.')
     version[2] = version[2].toInteger() + 1
     packageJson['version'] = version.join('.')
+    println "next version ${packageJson['version']}"
     writeJSON file: "package.json", json: packageJson, pretty: 4
 }
