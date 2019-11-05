df9e17ffc5d05038a79874fb182b514d4093177e
Or Evron <orevron@gmail.com>
2019-11-05 10:02:55 +0200

    change path

--- vars/accountFromProfile.groovy
+++ vars/accountFromProfile.groovy
@@ -4,5 +4,6 @@ def call(awsAccount) {
     def accountMapping = libraryResource "account_mapping_profile.json"
     def jsonSlurper = new JsonSlurper()
     def object = jsonSlurper.parseText(accountMapping)
+    println object[awsAccount]
     return object[awsAccount]
 }
