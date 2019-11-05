194efd8238d95771854c4a0fca6ff4f20199f46c
Or Evron <orevron@gmail.com>
2019-11-05 14:22:57 +0200

    testing calling function inside constructor

--- src/Kuku.groovy
+++ src/Kuku.groovy
@@ -1,12 +1,5 @@
 import groovy.json.JsonSlurper;
 
-def doSomething(awsAccount) {
-    def accountMapping = libraryResource "account_mapping_profile.json"
-    def jsonSlurper = new JsonSlurper()
-    def object = jsonSlurper.parseText(accountMapping)
-    return object[awsAccount]
-}
-
 class Kuki {
     def script
     def some
@@ -16,6 +9,13 @@ class Kuki {
         this.script = script
 //        this.some = accountFromProfile("dev")
         this.something = doSomething("dev")
+    }
+
+    static def doSomething(awsAccount) {
+        def accountMapping = libraryResource "account_mapping_profile.json"
+        def jsonSlurper = new JsonSlurper()
+        def object = jsonSlurper.parseText(accountMapping)
+        return object[awsAccount]
     }
 
     def print() {
