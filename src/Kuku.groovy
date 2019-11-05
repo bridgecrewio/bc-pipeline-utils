8e4bf3d60d0c75bc7f2434ddaa0223f221f6f860
Or Evron <orevron@gmail.com>
2019-11-05 14:24:37 +0200

    testing calling function inside constructor

--- src/Kuku.groovy
+++ src/Kuku.groovy
@@ -13,7 +13,7 @@ class Kuki {
 
     @NonCPS
     def doSomething(awsAccount) {
-        def accountMapping = libraryResource "account_mapping_profile.json"
+        def accountMapping = this.script.readJSON "account_mapping_profile.json"
         def jsonSlurper = new JsonSlurper()
         def object = jsonSlurper.parseText(accountMapping)
         return object[awsAccount]
