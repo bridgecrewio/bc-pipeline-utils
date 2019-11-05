3255f6c5993bb2b039da1450ad9c3017a1beaf88
Or Evron <orevron@gmail.com>
2019-11-05 14:55:08 +0200

    testing calling function inside constructor

--- src/Kuku.groovy
+++ src/Kuku.groovy
@@ -1,18 +1,10 @@
 class Kuku {
     def something
-    def accountFromProfile = [
-            "dev"  : "090772183824",
-            "test" : "148726905943",
-            "stage": "372188014275",
-            "acme3": "714018233037",
-            "acme" : "809694787632",
-            "prod" : "890234264427",
-            "root" : "986292867370",
-            "demo" : "418408045322"
-    ]
+
 
     Kuku() {
-        this.something = accountFromProfile["dev"]
+//        this.something = accountFromProfile["dev"]
+        this.something = ""
     }
 
     def print() {
