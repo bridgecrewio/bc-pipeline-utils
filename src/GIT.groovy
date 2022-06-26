d16e0ae46eec2a08014d8a480759abbafef32938
Nir Avni <navni@paloaltonetworks.com>
2022-06-26 15:31:17 +0300

    Update GIT.groovy

--- src/GIT.groovy
+++ src/GIT.groovy
@@ -43,11 +43,9 @@ class GitUtils {
             def keyPath = String.format('~/.ssh/id_rsa_%s', credentialsId)
             script.sh("""
                 cp ${script.ssh_key} ${keyPath}
-                iconv -c -f UTF8 -t ASCII ${keyPath} > ${keyPath}
-                openssl rsa -in ${keyPath} -text > tmp_id_rsa.pem
-                openssl pkcs8 -topk8 -v2 aes128 -in tmp_id_rsa.pem -out tmp_id_rsa.pem
-                chmod 600 tmp_id_rsa.pem
-                echo IdentityFile tmp_id_rsa.pem >> ~/.ssh/config
+                openssl pkcs8 -topk8 -v2 aes128 -in ${keyPath} -out ${keyPath}
+                chmod 400 ${keyPath}
+                echo IdentityFile ${keyPath} >> ~/.ssh/config
             """)
         }
     }
