ca0f87d402bb0032d135b2067cd1b2e056861734
Nir Avni <navni@paloaltonetworks.com>
2022-07-14 18:01:59 +0300

    Update GIT.groovy

--- src/GIT.groovy
+++ src/GIT.groovy
@@ -46,6 +46,7 @@ class GitUtils {
                 cp ${script.ssh_key} ${keyPath}
                 chmod 400 ${keyPath}
                 echo IdentityFile ${keyPath} >> ~/.ssh/config
+                echo StrictHostKeyChecking no >> ~/.ssh/config
             """)
         }
     }
